5e0156fcdee0ef27eab4d657ca7c2d02beae7d61
msaling <mattsaling1@Matts-MacBook-Pro.local>
2015-04-15 16:38:51 -0700

    Added schedule page

--- js/main.js
+++ js/main.js
@@ -14,5 +14,31 @@ $(function() {
         event.preventDefault();
         $('.mobile-nav').slideToggle();
     });
+    
+    $('.desc-toggle').click(function(e){
+        e.preventDefault();
+        $(this).find('i').toggleClass('fa-caret-right').toggleClass('fa-caret-down');
+        $(this).next().slideToggle();
+    });
+    
+    $('.track-title').click(function(e){
+        e.preventDefault();
+        $(this).find('i').toggleClass('fa-chevron-down').toggleClass('fa-chevron-up');
+        $(this).next().slideToggle();
+    });
+    
+    $('.open-all').click(function(e){
+        e.preventDefault();
+        $('.open-all, .close-all').toggle();
+        $('.track-title .fa').removeClass('fa-chevron-down').addClass('fa-chevron-up');
+        $('.track-session').show();
+    });
+    
+    $('.close-all').click(function(e){
+        e.preventDefault();
+        $('.open-all, .close-all').toggle();
+        $('.track-title .fa').removeClass('fa-chevron-up').addClass('fa-chevron-down');
+        $('.track-session').hide();
+    });
 
 });
